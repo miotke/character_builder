9d673310acd7a13f4e91314a17dd012ba614cde3
Andrew Miotke <andrewmiotke@gmail.com>
2021-05-17 10:23:09 -0700

    Adding methods for the plus and minus stat buttons

--- src/Components/CharacterBuilder.js
+++ src/Components/CharacterBuilder.js
@@ -7,21 +7,46 @@ class CharacterBuilder extends Component {
 
     state = { 
         characterName: "Character name should appear here",
+        availableAttributePoints: 20,
         characterStats: [ 
-            { attribute: "strength", value: 1},
+            { attribute: "strength", value: 0},
             { attribute: "intellect", value: 0},
             { attribute: "agility", value: 0},
             { attribute: 'Spirit', value: 0},
         ]
     }
 
+    handleAvailableAttributePoints = () => { 
+        console.log("available points " + this.state.availableAttributePoints);
+    }
+
+    handleAddStatPoint = () => { 
+        console.log('Add stats!');
+        // TODO: 
+        //  - Add one to value
+        //  - Subtract one from availableAttributePoints
+    }
+
+    handleSubtractStatPoint = () => { 
+        console.log("Remove stats!");
+        // TODO: 
+        //  - Subtract one from value
+        //  - Add one to availableAttributePoints
+    }
+
     render() { 
         return(
             <div>
                 <h2>Character Name: '{this.state.characterName}'</h2>
-
                 {this.state.characterStats.map(stats => { 
-                    return <StatCounter key={stats.id} characterStats={stats} />
+                    return <StatCounter 
+                                key={stats.attribute} 
+                                onAddStatPoint={this.handleAddStatPoint} 
+                                onSubtractStatPoint={this.handleSubtractStatPoint} 
+                                attPoints={this.state.availableAttributePoints}
+                                characterStats={stats} 
+                            />
+
                 })}
             </div>
         )
--- src/Components/StatCounter.js
+++ src/Components/StatCounter.js
@@ -1,40 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class StatCounter extends Component {
 
-    getAttribute() { 
+    getAttribute() {
         const { attribute } = this.props.characterStats;
         return attribute;
     }
 
-    getStat() { 
+    getStat() {
         const { value } = this.props.characterStats;
         return value;
     }
 
-    // TODO: subtractStatPoint and addStatPoint don't really do anything right now
-    // this needs to be fixed and the the actual work should be done by CharacterBuilder
-    subtractStatPoint = () => { 
-        const subtractStatPoint = this.props.characterStats.value - 1;
-        console.log("subtracting stat point");
-        return subtractStatPoint;
-    }
-
-    addStatPoint = () => { 
-        const addStatPoint = this.props.characterStats.value + 1;
-        console.log("adding stat point");
-        return addStatPoint;
-    }
-
-
     render() {
         return (
-            <div>
-                <h3>{this.getStat()}</h3>
-                <p>{this.getAttribute()}</p>
-                <button onClick={this.subtractStatPoint}>-</button>
-                <button onClick={this.addStatPoint}>+</button>
-            </div>
+            <Fragment>
+                <h4>Available attribute points: {this.props.attPoints}</h4>
+
+                <div>
+                    <h3>{this.getStat()}</h3>
+                    <p>{this.getAttribute()}</p>
+                    <button onClick={() => this.props.onSubtractStatPoint(this.props.characterStats.value)}>-</button>
+                    <button onClick={() => this.props.onAddStatPoint(this.props.characterStats.value)}>+</button>
+                </div>
+            </Fragment>
         )
     }
 }
