3576df32cf82a6be61e2f1f3b3a7d58e86337608
Andrew Miotke <andrewmiotke@gmail.com>
2021-05-17 16:09:09 -0700

    Character Race: Created a radio selector to select a characters race.
    - This is temporary until I can learn how to pass the state from another
      component.

--- src/Components/CharacterBuilder.js
+++ src/Components/CharacterBuilder.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import CharacterBackStory from './CharacterBackStory';
 
 // Component imports
 import StatCounter from './StatCounter';
+import CharacterBackStory from './CharacterBackStory';
+import CharacterRace from './ChracterRace';
 
 class CharacterBuilder extends Component { 
 
@@ -15,7 +16,8 @@ class CharacterBuilder extends Component {
             { attribute: 'agility', value: 0},
             { attribute: 'Spirit', value: 0},
         ],
-        characterBackStory: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        characterBackStory: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        characterRace: "",
     }
 
     handleAvailableAttributePoints = () => { 
@@ -36,6 +38,17 @@ class CharacterBuilder extends Component {
         //  - Add one to availableAttributePoints
     }
 
+    handleOptionChange = changeRace => { 
+        this.setState({
+            characterRace: changeRace.target.value
+        });
+
+    }
+
+    verifyRace = () => { 
+        console.log(this.state.characterRace)
+    }
+
     render() { 
         return(
             <div>
@@ -54,7 +67,27 @@ class CharacterBuilder extends Component {
 
                 })}
 
+                {/* <CharacterRace selectedRace={this.state.characterRace} onChange={this.handleOptionChange} /> */}
                 <CharacterBackStory backstory={this.state.characterBackStory} />
+
+                {/* TODO: This works but needs to be using the CharacterRace component.  */}
+                <p>----------------------------------------------------</p>
+                <h2>Testing Character Race selection</h2>
+                <form>
+                    <div className="form-check">
+                    <input type="radio" value="human" id="human" onChange={this.handleOptionChange} name="characterRace" />
+                    <label>Human</label>
+
+                    <input type="radio" value="orc" id="orc" onChange={this.handleOptionChange} name="characterRace" />
+                    <label>Orc</label>
+
+                    <input type="radio" value="troll" id="troll" onChange={this.handleOptionChange} name="characterRace" />
+                    <label>Troll</label>
+                    </div>
+                </form>
+
+
+                <button onClick={this.verifyRace}>Verify race</button>
             </div>
         )
     }
